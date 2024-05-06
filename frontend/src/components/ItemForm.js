9299d81a33a0b96cd3a7687a4ae665e1bc49cb7c
terrylu12212 <t.lu@ufl.edu>
2024-05-06 18:47:43 -0400

    Post ItemForm Details to DB and Allowed for Localhost proxy

--- frontend/src/components/ItemForm.js
+++ frontend/src/components/ItemForm.js
@@ -1,19 +1,49 @@
 import { useState } from "react"
 
 const ItemForm = () => {
+  const[title, setTitle] = useState('')
+  const[expirationDate, setExpirationDate] = useState('')
+  const[error, setError] = useState(null)
+
+  const handleSetTitle = (e) => {setTitle(e.target.value)}
+  const handleSetExpirationDate = (e) => {setExpirationDate(e.target.value)}
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const itemDetails={title, expirationDate}
+      const response = await fetch('/api/items', {
+        method: 'POST',
+        body: JSON.stringify(itemDetails),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const json = await response.json()
+
+    if(!response.ok) {
+        setError(json.error)
+    }
+    if(response.ok) {
+        setError(null)
+        console.log('new item details added', json)
+    }
+    setTitle('')
+    setExpirationDate('')
+  }
+
   return (
 
-   <form>
+   <form onSubmit={handleSubmit}>
 
     <h3>Item and Expiration Date Form</h3>
 
-    <label>Type Item Updated: </label>
-    <input type='text'/>
+    <label>Type Item: </label>
+    <input type='text' value={title} onChange={handleSetTitle} required/>
 
     <label>Enter Expiration Date Updated: </label>
-    <input type='date'/>
+    <input type='date' value={expirationDate} onChange={handleSetExpirationDate} required/>
 
-    <button>Submit</button>
+    <button type="submit">Submit</button>
    </form>
   )
 }
