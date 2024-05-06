1a21ae67bb1adfef90194b0f8ddec751d2d3a1e6
terrylu12212 <t.lu@ufl.edu>
2024-05-06 12:12:07 -0400

    added database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const itemRoutes = require('./routes/items')
 
 // express app
@@ -17,7 +18,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/items', itemRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
