2e7eaeacfcabcae4a948341d9f2db284d84f0a4b
terrylu12212 <t.lu@ufl.edu>
2024-05-06 13:43:12 -0400

    Added Item Form + Changed Date in Model

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import ItemCard from "./components/ItemCard/ItemCard";
+import ItemForm from"./components/ItemForm";
 
 function App() {
   return (
     <div className="App">
       <ItemCard />
+      <ItemForm></ItemForm>
     </div>
   );
 }
