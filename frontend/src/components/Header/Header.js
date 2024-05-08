439d8657a07e2288f436b4ceef68b3119c388e42
lrwilliams33 <116681252+lrwilliams33@users.noreply.github.com>
2024-05-07 23:54:32 -0400

    header and body styling

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p className="fresh-watch-txt">FreshWatch</p>
+    </div>
+  );
+}
+
+export default Header;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,13 @@
-//components to import into the home page
+import Header from "../components/Header/Header";
 import ItemCard from "../components/ItemCard/ItemCard";
 import ItemForm from"../components/ItemForm";
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className='home' style={{ display: 'flex', justifyContent: 'center' }}>
+        <Header />
         <ItemCard />
-        <ItemForm></ItemForm>
+        {/* <ItemForm></ItemForm> */}
     </div>
   )
 }
