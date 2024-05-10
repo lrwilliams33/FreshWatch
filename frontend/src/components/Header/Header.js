77c2efe74fdb0eacd208b8cd47335b692279b014
terrylu12212 <t.lu@ufl.edu>
2024-05-10 18:11:00 -0400

    Added useLogout hook

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import {useLogout} from '../../hooks/useLogout'
 import './Header.css'
 
 const Header = () => {
+  const {logout} = useLogout()
+  
+  const handleClick = () => {
+    logout()
+  }
   return (
     <header>
       <div className="container">
@@ -10,6 +16,9 @@ const Header = () => {
           <h1>FreshWatch</h1>
         </Link>
         <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
           <div>
             <Link to="/login">Login </Link>
             <Link to="/signup">Signup</Link>
