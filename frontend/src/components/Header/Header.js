0502e33efdd45199b69bd97948d46c4e8bb2219d
terrylu12212 <t.lu@ufl.edu>
2024-05-11 17:23:02 -0400

    Keeps Auth State After Refresh

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import {useLogout} from '../../hooks/useLogout'
+import {useAuthContext} from '../../hooks/useAuthContext'
 import './Header.css'
 
 const Header = () => {
   const {logout} = useLogout()
-  
+  const{user} = useAuthContext()
   const handleClick = () => {
     logout()
   }
@@ -16,13 +17,18 @@ const Header = () => {
           <h1>FreshWatch</h1>
         </Link>
         <nav>
+          {user && (
           <div>
+            <span>{user.email}</span>
             <button onClick={handleClick} className='button'>Log out</button>
           </div>
+          )}
+          {!user && (
           <div>
             <Link to="/login">Login </Link>
             <Link to="/signup">Sign Up</Link>
           </div>
+          )}
         </nav>
       </div>
     </header>
